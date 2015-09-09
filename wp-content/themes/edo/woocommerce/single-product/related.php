0bb7a061b6476ce3e33134f93bf3a5a0c9847396
ngocthangict <ngocthang.ict@gmail.com>
2015-09-09 17:09:40 +0700

    Update single product

---
+++ wp-content/themes/edo/woocommerce/single-product/related.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Related Products
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $product, $woocommerce_loop;
+
+if ( empty( $product ) || ! $product->exists() ) {
+	return;
+}
+
+$related = $product->get_related( $posts_per_page );
+
+if ( sizeof( $related ) == 0 ) return;
+
+$args = apply_filters( 'woocommerce_related_products_args', array(
+	'post_type'            => 'product',
+	'ignore_sticky_posts'  => 1,
+	'no_found_rows'        => 1,
+	'posts_per_page'       => $posts_per_page,
+	'orderby'              => $orderby,
+	'post__in'             => $related,
+	'post__not_in'         => array( $product->id )
+) );
+
+$products = new WP_Query( $args );
+
+$woocommerce_loop['columns'] = $columns;
+
+if ( $products->have_posts() ) : ?>
+
+	<!-- Up-sell -->
+	<div class="block block-products-owl">
+		<div class="block-head">
+			<div class="block-title">
+				<div class="block-title-text text-lg"><?php _e( 'Related Products', 'woocommerce' ) ?></div>
+			</div>
+		</div>
+		<div class="block-inner">
+			<ul class="products kt-owl-carousel" data-margin="20" data-loop="true" data-nav="true" data-responsive='{"0":{"items":1},"600":{"items":3},"1000":{"items":5}}'>
+
+				<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+
+					<?php wc_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</ul>
+		</div>
+	</div>
+	<!-- .Up-sell -->
+
+<?php endif;
+
+wp_reset_postdata();
