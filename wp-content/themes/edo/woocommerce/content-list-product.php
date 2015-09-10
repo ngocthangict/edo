7b8bc351d1e48972b96e2f7218c13e67aba2c5a4
AngelsIT <thanhtd91@gmail.com>
2015-09-11 00:02:44 +0700

    Content list product

--- wp-content/themes/edo/woocommerce/content-list-product.php
+++ wp-content/themes/edo/woocommerce/content-list-product.php
@@ -12,98 +12,100 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div class="product-container">
-	<div class="product-left">
-		<div class="product-thumb">
-			<a class="product-img" href="<?php the_permalink() ?>">
-                <?php
-        			/**
-        			 * woocommerce_template_loop_product_thumbnail hook
-        			 *
-        			 * @hooked edo_wc_loop_product_thumbnail - 10
+    <div class="inner">
+    	<div class="product-left">
+    		<div class="product-thumb">
+    			<a class="product-img" href="<?php the_permalink() ?>">
+                    <?php
+            			/**
+            			 * woocommerce_template_loop_product_thumbnail hook
+            			 *
+            			 * @hooked edo_wc_loop_product_thumbnail - 10
+            			 */
+            			do_action( 'edo_wc_loop_product_thumbnail' );
+            		?>
+                </a>
+    			<?php 
+                    /**
+                     * edo_get_tool_quickview hook
+                     * 
+                     * @hook edo_wc_loop_function_quickview
+                     * */
+                    do_action( 'edo_wc_loop_function_quickview' ) 
+                ?>
+    		</div>
+    		<div class="product-status">
+                <?php 
+                /**
+        			 * edo_wc_loop_product_label hook
+                     * 
+        			 * @hooked edo_show_product_loop_new_flash - 5
+        			 * @hooked woocommerce_show_product_loop_sale_flash - 10
         			 */
-        			do_action( 'edo_wc_loop_product_thumbnail' );
-        		?>
-            </a>
-			<?php 
-                /**
-                 * edo_get_tool_quickview hook
-                 * 
-                 * @hook edo_wc_loop_function_quickview
-                 * */
-                do_action( 'edo_wc_loop_function_quickview' ) 
-            ?>
-		</div>
-		<div class="product-status">
-            <?php 
-            /**
-    			 * edo_wc_loop_product_label hook
-                 * 
-    			 * @hooked edo_show_product_loop_new_flash - 5
-    			 * @hooked woocommerce_show_product_loop_sale_flash - 10
+        			do_action( 'edo_wc_loop_product_label' );
+                 ?>
+    		</div>
+    	</div>
+    	<div class="product-right">
+    		<div class="product-name">
+                <?php 
+    			/**
+    			 * woocommerce_shop_loop_item_title hook
+    			 *
+    			 * @hooked woocommerce_template_loop_product_title - 10
     			 */
-    			do_action( 'edo_wc_loop_product_label' );
-             ?>
-		</div>
-	</div>
-	<div class="product-right">
-		<div class="product-name">
+    			do_action( 'woocommerce_shop_loop_item_title' );
+                ?>
+    		</div>
+    		<div class="price-box">
+    			<?php 
+                    /**
+                     * edo_wc_loop_product_price hook
+                     * 
+                     * @hook woocommerce_template_loop_price - 10
+                     * */
+                    do_action( 'edo_wc_loop_product_price' )
+                 ?>
+    		</div>
             <?php 
-			/**
-			 * woocommerce_shop_loop_item_title hook
-			 *
-			 * @hooked woocommerce_template_loop_product_title - 10
-			 */
-			do_action( 'woocommerce_shop_loop_item_title' );
-            ?>
-		</div>
-		<div class="price-box">
-			<?php 
                 /**
-                 * edo_wc_loop_product_price hook
+                 * edo_wc_loop_product_rating hook
                  * 
-                 * @hook woocommerce_template_loop_price - 10
+                 * @hook woocommerce_template_loop_rating - 10
                  * */
-                do_action( 'edo_wc_loop_product_price' )
+                do_action( 'edo_wc_loop_product_rating' )
              ?>
-		</div>
-        <?php 
-            /**
-             * edo_wc_loop_product_rating hook
-             * 
-             * @hook woocommerce_template_loop_rating - 10
-             * */
-            do_action( 'edo_wc_loop_product_rating' )
-         ?>
-		
-        <div class="product-button">
-            <?php
-        		/**
-        		 * edo_wc_loop_product_compare hook
-        		 *
-        		 * @hooked edo_get_tool_wishlish - 10
-        		 */
-        		do_action( 'edo_wc_loop_product_wishlish' );
-        
-        	?>
+    		
+            <div class="product-button">
+                <?php
+            		/**
+            		 * edo_wc_loop_product_compare hook
+            		 *
+            		 * @hooked edo_get_tool_wishlish - 10
+            		 */
+            		do_action( 'edo_wc_loop_product_wishlish' );
             
-            <?php
-        		/**
-        		 * edo_wc_loop_product_compare hook
-        		 *
-        		 * @hooked edo_get_tool_compare - 10
-        		 */
-        		do_action( 'edo_wc_loop_product_compare' );
-        
-        	?>
+            	?>
+                
+                <?php
+            		/**
+            		 * edo_wc_loop_product_compare hook
+            		 *
+            		 * @hooked edo_get_tool_compare - 10
+            		 */
+            		do_action( 'edo_wc_loop_product_compare' );
             
-            <?php
-        		/**
-        		 * woocommerce_after_shop_loop_item hook
-        		 *
-        		 * @hooked woocommerce_template_loop_add_to_cart - 10
-        		 */
-        		do_action( 'woocommerce_after_shop_loop_item' );
-        	?>
-        </div>
-	</div>
+            	?>
+                
+                <?php
+            		/**
+            		 * woocommerce_after_shop_loop_item hook
+            		 *
+            		 * @hooked woocommerce_template_loop_add_to_cart - 10
+            		 */
+            		do_action( 'woocommerce_after_shop_loop_item' );
+            	?>
+            </div>
+    	</div>
+    </div>
 </div>
